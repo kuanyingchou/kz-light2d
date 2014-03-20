2a33dc3de689e7bba4b156007821e2dca5215e2f
ken <kuanying@tradeserv.com>
2014-03-20 08:59:31 +0800

    add event tests

---
+++ Assets/TestEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEvent : MonoBehaviour {
+    public void EnterLight() {
+        Debug.Log("enter light");
+    }
+    public void LeaveLight() {
+        Debug.Log("leave light");
+    }
+}
